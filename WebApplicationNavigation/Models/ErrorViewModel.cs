9eaf99d9a81d73bc89184e8df0f501f75512feaa
Mkreios <marwen.ksontini.ti@gmail.com>
2025-12-14 18:03:11 +0100

    fix commit

--- WebApplicationNavigation/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplicationNavigation.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
